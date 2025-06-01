4a2a576f3703f068b1d7e3389bb77eeb598dba6c
seanwesbrown <seanwesbrown@gmail.com>
2025-06-01 11:57:49 -0600

    BSF-005 | updating front page (#6)

--- src/main/frontend/views/@index.tsx
+++ src/main/frontend/views/@index.tsx
@@ -1,6 +1,6 @@
-// TODO Replace with your own main view.
-
+import { useNavigate } from 'react-router';
 import { ViewConfig } from '@vaadin/hilla-file-router/types.js';
+import { Button, VerticalLayout } from '@vaadin/react-components';
 
 export const config: ViewConfig = {
   menu: {
@@ -9,5 +9,26 @@ export const config: ViewConfig = {
 };
 
 export default function MainView() {
-  return <main className="p-m">Please select a view from the menu on the left.</main>;
+  const navigate = useNavigate();
+
+  return (
+    <main className="h-full">
+      <div className="bobby-main-layout h-full">
+        <div className="bobby-main-left p-m">
+          <VerticalLayout className="bobby-title p-m">
+            <h1>Bobbysoft Organizer</h1>
+            <p>Welcome to Bobbysoft Organizer, the modular organizer.</p>
+            <Button key="login" theme="primary" onClick={() => navigate('/login')}>
+              Login
+            </Button>
+            <Button key="signup" onClick={() => navigate('/signup')}>
+              Sign Up
+            </Button>
+          </VerticalLayout>
+        </div>
+        <VerticalLayout className="bobby-main-right" />
+        <div className="bobby-background"></div>
+      </div>
+    </main>
+  );
 }
